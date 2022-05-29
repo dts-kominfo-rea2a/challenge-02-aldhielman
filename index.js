0b4452221f4db8c801a1746b8fabf00e15fecc4c
Thoriq Rivaldi Novendra <rivaldithoriq@gmail.com>
2022-05-29 21:00:18 +0700

    finish task

--- index.js
+++ index.js
@@ -25,17 +25,39 @@ function lakukanLooping(arrPegawai) {
   */
   let hasilLooping = null;
 
+  hasilLooping = [];
+
+  for (const pegawai of dataYangAkanDilooping) {
+    hasilLooping.push(pegawai.namaDepan + " " + pegawai.namaBelakang)
+  }
+
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
 
+  jumlahPria = 0
+
+  for (const pegawai of dataYangAkanDilooping) {
+    if (pegawai.jenisKelamin.toUpperCase() == "M") {
+      jumlahPria++
+    }
+  }
+
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
+
+  jumlahWanita = 0
+
+  for (const pegawai of dataYangAkanDilooping) {
+    if (pegawai.jenisKelamin.toUpperCase() == "F") {
+      jumlahWanita++
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -49,6 +71,16 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita"
+  }
+  else if (jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  }
+  else {
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
